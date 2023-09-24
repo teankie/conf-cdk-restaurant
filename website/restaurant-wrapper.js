7cbca9d09ca969967860160ddc050cf2847de722
randyvroegop <randy.vroegop@luminis.eu>
2023-09-24 11:04:11 +0200

    Add website files

---
+++ website/restaurant-wrapper.js
@@ -0,0 +1,34 @@
+// Import the LitElement base class and html helper function
+import { LitElement, html, css } from 'lit-element';
+
+// Extend the LitElement base class
+class RestaurantWrapper extends LitElement {
+
+    static styles = css`
+    :host {
+      display: block;
+      padding: 16px;
+    }
+  `;
+
+    // Define a property
+    static properties = {
+        message: { type: String }
+    };
+
+    constructor() {
+        super();
+        // Initialize the message property
+        this.message = 'Hello from restaurant-wrapper!';
+    }
+
+    // Render the element's DOM
+    render() {
+        return html`
+      <p>${this.message}</p>
+    `;
+    }
+}
+
+// Register the element with the browser
+customElements.define('restaurant-wrapper', RestaurantWrapper);
